311dd83e300e3a6a198d2fd92d71c6d89a805a18
eric <ericsong19990917@gmail.com>
2023-02-14 10:58:24 +0900

    mesaage

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "eric-lb2-sg"{
         from_port =0
         to_port=65535
         protocol="tcp"
-        cidr_blocks = ["27.122.140.10/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress{
         from_port =0
